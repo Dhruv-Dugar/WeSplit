21e2efb28ab0373b2e5c7d0e5a78cbd7889d4c44
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-10 17:33:02 +0530

    adding picker and checkamt field

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,17 +5,35 @@
 //  Created by Dhurv Dugar on 10/01/23.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
     
-    var tapCount = 0
+    let tipPercentages = [0, 10, 15, 25, 40]
     
-    var body: some View {
-        Button("Tap Count :\(tapCount)"){
-            tapCount += 1
+    var body: some View{
+        Form{
+            Section{
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "INR"))
+                    .keyboardType(.decimalPad)
+                                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100){
+                        Text("\($0)")
+                    }
+                }
+            }
+            
+            Section {
+                   Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "INR"))
+               }
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
