adb943a0a181a80055b432a65bb2a2772ddefcf4
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-10 15:21:04 +0530

    inital commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dhurv Dugar on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
