298954bfcbe3c1cca445495fc1106d03531f7719
Dhruv-Dugar <dhruvdugar02@gmail.com>
2023-01-10 15:45:06 +0530

    making changes to project

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var tapCount = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap Count :\(tapCount)"){
+            tapCount += 1
         }
-        .padding()
     }
 }
 
